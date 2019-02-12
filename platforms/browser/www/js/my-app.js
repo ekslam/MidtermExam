1e9fcb31c2e8c2da5ed8da81ee0fdaf980b9846a
Kevin <edwardkevinl1@gmail.com>
2019-02-12 17:11:11 +0800

    Changes in Candles with JS and HTML + Cards

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -201,3 +201,150 @@ var app = new Framework7({
   $$('.popup-prompto').on('popup:open', function (e, popup) {
     console.log('Profile Page');
   });
+
+
+
+  //----------------  18 CANDLES  ---------------//
+  //Tifa Lockhart
+  $$('.popup-tifa').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-tifa').on('popup:opened', function (e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Yuffie
+  $$('.popup-yuffie').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-yuffie').on('popup:opened', function (e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Lunafreya
+  $$('.popup-luna').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-luna').on('popup:opened', function (e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Iris
+  $$('.popup-iris').on('popup:open', function (e, popup){
+    console.log('Profile Page');
+  });
+  $$('.popup-iris').on('popup:opened', function(e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Yuna
+  $$('.popup-yuna').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-yuna').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Lightning
+  $$('.popup-lightning').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-lightning').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Terra
+  $$('.popup-terra').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-terra').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Rinoa
+  $$('.popup-rinoa').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-rinoa').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Vanille
+  $$('.popup-vanille').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-vanille').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Serah
+  $$('.popup-serah').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-serah').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Rikku
+  $$('.popup-rikku').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-rikku').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Rydia
+  $$('.popup-rydia').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-rydia').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Penelo
+  $$('.popup-penelo').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-penelo').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Rhul
+  $$('.popup-rhul').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-rhul').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Selphie
+  $$('.popup-selphie').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-selphie').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Rem
+  $$('.popup-rem').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-rem').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Ashe
+  $$('.popup-ashe').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-ashe').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+
+  //Deuce
+  $$('.popup-deuce').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
+  $$('.popup-deuce').on('popup:open', function (e, popup) {
+    console.log('Profile Page');
+  });
